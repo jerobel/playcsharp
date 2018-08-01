2d626bb9e7f6f5dc663afa552da802591ef0d81f
kerobs <gjerobs@gmail.com>
2018-08-01 10:29:02 +0800

    add local files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program {
+
+    public static int Fibonacci(int n) {
+        int a = 0;
+        int b = 1;
+
+        for(int i = 0; i < n; i++) {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a;
+    }
+
+    static void Main() {
+        for(int i = 0; i < 30; i++) {
+            Console.WriteLine(Fibonacci(i));
+        }
+    }
+}
